42301e89458c0466c8fc09a39906e39e144a2017
BigDkNo <163581456+BigDkNo@users.noreply.github.com>
2025-06-16 12:27:04 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,23 @@
-self.addEventListener("install", e => {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open("restart-edu").then(cache => {
+    caches.open('restart-cache').then(cache => {
       return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifesto.html",
-        "/lectia1.html",
-        "/icon.png"
+        '/',
+        '/index.html',
+        '/manifesto.html',
+        '/lectia1.html',
+        '/lectia2.html',
+        '/style.css',
+        '/manifest.json',
+        '/icon.png'
       ]);
     })
   );
 });
-
-self.addEventListener("fetch", e => {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
   );
 });
